42baca4b5b2e148a750ff63ac82a918118826a6d
Garrett Holtz <gholtz67@gmail.com>
2023-09-14 12:12:21 -0400

    Updated clustering to included r2 and RMSE

--- Final Project/Clustering.py
+++ Final Project/Clustering.py
@@ -18,18 +18,31 @@ from sklearn.feature_selection import f_classif
 
 # Model Development
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import precision_recall_curve
 from sklearn.cluster import KMeans
+
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 # Global Variables
 
 def main():
+    print("Processing csv")
     processedData = processCSV()
+    
+    print("\nDetermining num of clusters")
     determineClusterNum(processedData)
+    
+    print("\nRunning kmeans with no target")
     dataC0, dataC1, dataC2, dataC3, kmeans = runKMeans(processedData)
+    
+    print("\nCreating scatters")
     createScatters(dataC0, dataC1, dataC2, dataC3, kmeans)
+    
+    print("\nRunning kmeans with target")
+    dataStats(processedData)
     return 0
 
 def processCSV():
@@ -39,15 +52,14 @@ def processCSV():
     np.set_printoptions(suppress=True)
     
     data_dummies = pd.get_dummies(data)
-    print(data_dummies.columns, "\n")
+    print(data_dummies.columns)
     
     return data_dummies
 
 def determineClusterNum(data):
     wcss = []
     for i in range(1,10):
-        k_means = KMeans(n_clusters=i,
-                         init='k-means++', 
+        k_means = KMeans(n_clusters=i, 
                          random_state=42, 
                          n_init='auto')
         k_means.fit(data)
@@ -56,14 +68,32 @@ def determineClusterNum(data):
     plt.plot(np.arange(1,10), wcss)
     plt.xlabel('Clusters')
     plt.ylabel('SSE')
+    plt.title("Elbow Curve w/ no Target")
     plt.show()
+    
+    kmeans = KMeans()
+    parameters = {
+        'n_clusters': [2, 3, 4, 5, 6, 7, 8, 9 , 10],
+        'random_state': [42],
+        'n_init':['auto']
+    }
+    
+    grid_search = GridSearchCV(kmeans, parameters, cv=5)
+    
+    grid_search.fit(data)
+    
+    print(grid_search.best_params_)
+    
     return 0
 
 def runKMeans(data):
     kmeans = KMeans(n_clusters=3, random_state=42, n_init='auto')
     kmeans.fit(data)
 
-    plt.hist(kmeans.labels_, facecolor="blue")
+    plt.hist(kmeans.labels_, facecolor="blue", align='mid', orientation='horizontal', bins=5)
+    plt.yticks([0,1,2])
+    plt.xlabel("Number of Occurrences")
+    plt.title("Clusters Based off ValStats")
     plt.show()
 
     clusters = kmeans.predict(data)
@@ -188,6 +218,59 @@ def createScatters(dataC0, dataC1, dataC2, dataC3, kmeans):
     plt.ylabel("Y")
     plt.title("Gun1 Kills vs First Bloods vs Assists")
     plt.show()
+    
+    return 0
+
+def dataStats(data):
+    data_features = data.drop(columns = ['kd_ratio'])
+    
+    DATA_TRAIN, DATA_TEST, TARGET_TRAIN, TARGET_TEST = train_test_split(
+                                                       data_features, 
+                                                       data['kd_ratio'],
+                                                       test_size = 0.7, 
+                                                       random_state = 42)
+    
+    wcss = []
+    for i in range(1,10):
+        kmeans = KMeans(n_clusters=i,
+                         random_state=42, 
+                         n_init='auto')
+        kmeans.fit(DATA_TRAIN, TARGET_TRAIN)
+        wcss.append(kmeans.inertia_)
+    #plot elbow curve
+    plt.plot(np.arange(1,10), wcss)
+    plt.xlabel('Clusters')
+    plt.ylabel('SSE')
+    plt.title("Elbow Curve w/ Target")
+    plt.show()
+    
+    # Run kmeans on the train data set
+    kmeans = KMeans(n_clusters=3, random_state=42, n_init='auto')
+    kmeans.fit(DATA_TRAIN, TARGET_TRAIN)
+
+    plt.hist(kmeans.labels_, facecolor="blue", align='mid', orientation='horizontal', bins=5)
+    plt.yticks([0,1,2])
+    plt.xlabel("Number of Occurrences")
+    plt.title("Clusters Based off ValStats")
+    plt.show()
+    
+    count = 0
+    for num in range(len(kmeans.cluster_centers_[0])):
+        num1 = kmeans.cluster_centers_[0][num]
+        num2 = kmeans.cluster_centers_[1][num]
+        num3 = kmeans.cluster_centers_[2][num]
+        if max(num1, num2, num3) - min(num1, num2, num3) > 10:
+            count+=1
+            print(data.columns[num])
+            print(num1, 
+                  num2, 
+                  num3, '\n')
+    print("Num of features", count)
+    
+    print(kmeans.score(DATA_TEST, TARGET_TEST))
+    
+    print(r2_score(TARGET_TEST, kmeans.predict(DATA_TEST)))
+    print(np.sqrt(mean_squared_error(TARGET_TEST, kmeans.predict(DATA_TEST))))
     
     return 0
 
